11f2a21bd5a5eef940c3028d287ffcaf2b128ba3
1luk0 <isabel.benitez46442@ucaldas.edu.co>
2024-11-02 22:12:53 -0500

    creacion segunda fase

---
+++ database/migrations/12_contratos.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'contratos'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.date('fechaInicio').notNullable
+      table.date('fechaFin').notNullable
+      table.string('id_Cliente').unsigned().references("clientes.id")
+
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
