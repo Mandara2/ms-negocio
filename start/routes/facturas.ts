83f0936dfd00f3b31bd2d8966c9314c6c31425c9
Yahir Rangel <yahirandres.rangel@gmail.com>
2024-12-13 20:40:43 -0500

    factura

--- start/routes/facturas.ts
+++ start/routes/facturas.ts
@@ -1,4 +1,5 @@
 import Route from "@ioc:Adonis/Core/Route";
+
 Route.group(() => {
   Route.get("/facturas", "FacturasController.find");
   Route.get("/facturas/:id", "FacturasController.find");
@@ -6,4 +7,7 @@ Route.group(() => {
   Route.post("/facturas", "FacturasController.create");
   Route.put("/facturas/:id", "FacturasController.update");
   Route.delete("/facturas/:id", "FacturasController.delete");
-})
+
+  // Nueva ruta para actualizar el estado de la factura a 'PAGADO'
+  Route.post("/facturas/:id/actualizar", "FacturasController.actualizarEstado");
+});
